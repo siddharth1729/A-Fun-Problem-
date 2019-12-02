f66d74a83aeeafbd8efe564f2ae7b92202362d0b
siddharth pandey <siddharth.sabron@gmail.com>
2019-12-01 22:01:55 -0500

    Printing the desired pattern .
    This is a small part of fun problem of mathematics in which the main part was to print the desired pattern . Basically in this part of code the program does a beautiful job which is it takes input from

---
+++ funproblem.c
@@ -0,0 +1,19 @@
+#include<stdio.h>                             //including header file 
+int main()                                    // starting of main() function 
+{
+    int c;                                     // variable declaration 
+    printf("\n enter any number ::  ");
+    scanf("%d",&c);                            // scanning the int number from user
+    if (c == 0)                                //logic
+        return 0;
+    while (c!=1)                               //logic
+    {
+        if (c % 2 == 0)
+            c=c/2;
+        else
+            c=c*3+1;
+        printf("-->");
+        printf("%d",c);                         // printing the pattern in desired foam 
+    }
+    printf("\n");
+}
